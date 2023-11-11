7a2a72bbd948bc24d086ed3480f1c8ebc9dd9a1d
motasimshahid <muhammadmotasimbin.shahid@gmail.com>
2023-11-12 00:15:20 +0100

    Work on task to process xml file and store data in database

--- app/Console/Commands/ProcessXmlData.php
+++ app/Console/Commands/ProcessXmlData.php
@@ -3,6 +3,7 @@
 namespace App\Console\Commands;
 
 use App\Models\DataEntry;
+use Exception;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\Log;
 
@@ -24,38 +25,71 @@ class ProcessXmlData extends Command
 
     /**
      * Execute the console command.
+     * @return void
+     * @throws Exception
      */
     public function handle()
     {
         try {
 
+            /**
+             * Getting the file path from the command line argument
+             */
             $file = $this->argument('file');
 
+            /**
+             * Checking if the file exists
+             */
             if (!file_exists($file)) {
+                /**
+                 * If the file does not exist, log an error and display an error message
+                 */
                 Log::error("The file {$file} does not exist.");
                 $this->error("The file {$file} does not exist.");
                 return;
             }
 
+
+            /**
+             * Loading the XML file
+             */
             $xml = simplexml_load_file($file);
 
+            /**
+             * Checking if the file is a valid XML file
+             */
             if (!$xml) {
+                /**
+                 * If the file is not a valid XML file, log an error and display an error message
+                 */
                 Log::error("The file {$file} is not a valid XML file.");
                 $this->error("The file {$file} is not a valid XML file.");
                 return;
             }
 
+            /**
+             * Converting the XML data to JSON
+             */
             $json = json_encode($xml);
 
 
+            /**
+             * Storing the XML data in the database
+             */
             DataEntry::create([
                 'xml_data' => $json,
             ]);
 
+            /**
+             * Log a success message and display a success message
+             */
             Log::info("The XML data from {$file} has been processed and stored in the database.");
             $this->info("The XML data from {$file} has been processed and stored in the database.");
 
-        } catch (\Exception $e) {
+        } catch (Exception $e) {
+            /**
+             * Log an error and display an error message
+             */
             Log::error($e->getMessage());
             $this->error("An error occurred while processing the XML data: {$e->getMessage()}");
         }
--- tests/Feature/XmlProcessCommandTest.php
+++ tests/Feature/XmlProcessCommandTest.php
@@ -23,18 +23,37 @@ class XmlProcessCommandTest extends TestCase
 
     public function setUp(): void
     {
+        /**
+         * Call the parent setup method otherwise the database will not be migrated
+         */
         parent::setUp();
 
+        /**
+         * Migrate the database
+         */
         $this->artisan('migrate');
     }
 
+    /**
+     * @test
+     */
     public function test_it_can_process_xml_file_and_save_data_to_database(): void
     {
+        /**
+         * Defining the path to the XML file within the base path
+         */
         $file = base_path('feed.xml');
+
+        /**
+         * Run the command and assert that the output matches the expected output
+         */
         $this->artisan('xml:process', [
             'file' => $file,
         ])->expectsOutput("The XML data from {$file} has been processed and stored in the database.")->assertExitCode(0);
 
+        /**
+         * Assert that the database has one record
+         */
         $this->assertDatabaseCount('data_entries', 1, 'mysql');
     }
 
